248799b1e0046890927ffc79818503ee635b2fb6
daxue0929 <daxue0929@qq.com>
2024-03-06 11:38:08 +0800

    统计返回体,全局异常处理

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -2,6 +2,7 @@ package com.atguigu.cloud.controller;
 
 import com.atguigu.cloud.entities.Pay;
 import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.resp.ResultData;
 import com.atguigu.cloud.service.PayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,36 +27,36 @@ public class PayController {
 
     @PostMapping(value = "/pay/add")
     @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
-    public String addPay(@RequestBody Pay pay) {
+    public ResultData<String> addPay(@RequestBody Pay pay) {
         log.info(pay.toString());
         int i = payService.add(pay);
-        return "成功插入记录，返回值: " + i;
+        return ResultData.success("成功插入记录，返回值: " + i);
     }
 
     @DeleteMapping(value = "/pay/del/{id}")
     @Operation(summary = "删除",description = "删除支付流水方法")
-    public Integer deletePay(@PathVariable("id") Integer id) {
-        return payService.delete(id);
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id) {
+        return ResultData.success(payService.delete(id));
     }
 
     @PutMapping(value = "/pay/update")
     @Operation(summary = "修改",description = "修改支付流水方法")
-    public String updatePay(@RequestBody PayDTO payDTO) {
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO) {
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
         int i = payService.update(pay);
-        return "成功修改记录，返回值: " + i;
+        return ResultData.success("成功修改记录，返回值: " + i);
     }
 
     @GetMapping(value = "/pay/get/{id}")
     @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
-    public Pay getById(@PathVariable("id") Integer id) {
-        return payService.getById(id);
+    public ResultData<Pay> getById(@PathVariable("id") Integer id) {
+        return ResultData.success(payService.getById(id));
     }
 
     @GetMapping(value = "/pay/getAll")
-    public List<Pay> getAll() {
+    public ResultData<List<Pay>> getAll() {
         List<Pay> all = payService.getAll();
-        return all;
+        return ResultData.success(all);
     }
 }
