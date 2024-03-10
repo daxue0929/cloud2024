0e2db3dfa1cf0b6bf9ac6fd412d5d32f7184d8a5
daxue0929 <daxue0929@qq.com>
2024-03-10 19:34:23 +0800

    Spring cloud集成Micrometer and zipkin

---
+++ cloud-consumer-feign-order81/src/main/java/com/atguigu/cloud/controller/OrderMicrometerController.java
@@ -0,0 +1,26 @@
+package com.atguigu.cloud.controller;
+
+import com.atguigu.cloud.apis.PayFeignApi;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author：daxue0929
+ * @Date：2024/3/10 19:19
+ */
+@RestController
+@Slf4j
+public class OrderMicrometerController
+{
+    @Resource
+    private PayFeignApi payFeignApi;
+
+    @GetMapping(value = "/feign/micrometer/{id}")
+    public String myMicrometer(@PathVariable("id") Integer id)
+    {
+        return payFeignApi.myMicrometer(id);
+    }
+}
