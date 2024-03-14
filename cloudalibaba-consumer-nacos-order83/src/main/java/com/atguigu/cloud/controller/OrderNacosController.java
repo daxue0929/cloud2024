8d78442a3809dd802b3f4baffb89f5ad2ed7f8ae
daxue0929 <daxue0929@qq.com>
2024-03-14 16:04:10 +0800

    集成nacos配置中心，改造83服务

--- cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/controller/OrderNacosController.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/controller/OrderNacosController.java
@@ -2,6 +2,7 @@ package com.atguigu.cloud.controller;
 
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,18 +13,26 @@ import org.springframework.web.client.RestTemplate;
  * @Date：2024/3/14 15:28
  */
 @RestController
+@RefreshScope //在控制器类加入@RefreshScope注解使当前类下的配置支持Nacos的动态刷新功能。
 public class OrderNacosController
 {
     @Resource
     private RestTemplate restTemplate;
 
+    @Value("${config.info}")
+    private String configInfo;
+
     @Value("${service-url.nacos-user-service}")
     private String serverURL;
 
     @GetMapping("/consumer/pay/nacos/{id}")
-    public String paymentInfo(@PathVariable("id") Integer id)
-    {
+    public String paymentInfo(@PathVariable("id") Integer id) {
         String result = restTemplate.getForObject(serverURL + "/pay/nacos/" + id, String.class);
         return result+"\t"+"    我是OrderNacosController83调用者。。。。。。";
+    }
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
     }
 }
